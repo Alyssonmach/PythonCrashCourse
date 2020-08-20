a14f25cc13823828846f28b6a69207bd2c45bd7c
Alyssonmach <alyssonmachado388@gmail.com>
2020-08-20 10:56:26 -0300

    Update Files

--- archive b - introduction/prog03-help.py
+++ archive b - introduction/prog03-help.py
@@ -2,5 +2,4 @@
 # and functions / methods available for a given type of data or variable.
 
 help("string".upper) # visualizes what the function does
-
 print('string'.upper()) # shows what the function does
