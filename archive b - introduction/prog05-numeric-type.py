2bd9ab147e76a5c2e313919ef8aacd45f1623dd2
Alyssonmach <alyssonmachado388@gmail.com>
2020-08-20 10:16:46 -0300

    Create program 05

---
+++ archive b - introduction/prog05-numeric-type.py
@@ -0,0 +1,19 @@
+# data types in python
+
+#creating different types of data
+data1 = 12
+data2 = 3.4
+data3 = 'string'
+data4 = ['dogs', 'cats']
+data5 = ('random', 3)
+data6 = {'cat': ['live', 'death'], 'light': ['particle', 'wave']}
+data7 = 7j
+
+# viewing data types
+print(type(data1), data1)
+print(type(data2), data2)
+print(type(data3), data3)
+print(type(data4), data4)
+print(type(data5), data5)
+print(type(data6), data6)
+print(type(data7), data7)
