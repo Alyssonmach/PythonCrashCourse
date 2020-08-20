b3ee476c50aad69957142bb5ccf78e150564d453
Alyssonmach <alyssonmachado388@gmail.com>
2020-08-20 09:01:22 -0300

    Create program 01

---
+++ archive a - good practices with python/prog01-zen-of-python.py
@@ -0,0 +1,6 @@
+# Python Zen is a collection of 19 guiding principles, 
+# in the form of a poem, with a series of aphorisms, 
+# for writing computer programs that influence the design 
+# of the Python programming language.
+
+import this
