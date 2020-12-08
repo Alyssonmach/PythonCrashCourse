f47ca5163ac6a13135d8aed16bab7b8e612951a8
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-08 16:26:38 -0300

    reiniciando arquivos

--- Archive A - Good Practices with Python/prog01-zen-of-python.py
+++
@@ -1,6 +0,0 @@
-# Python Zen is a collection of 19 guiding principles, 
-# in the form of a poem, with a series of aphorisms, 
-# for writing computer programs that influence the design 
-# of the Python programming language.
-
-import this
--- Archive B - Introduction/learn with codes/prog02-dir.py
+++
@@ -1,8 +0,0 @@
-# displays all available attributes/properties and functions/methods
-
-print(dir("string")) # viewing in string
-print('\n')
-print(dir(2)) # viewing in int
-print('\n')
-print(dir(3.4)) # viewing in float
-
