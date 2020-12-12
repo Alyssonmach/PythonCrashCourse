0ffce75a281b7603860fe72f88ecff4a66be7e50
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:51:50 -0300

    Create ex4

---
+++ Conteúdo B - Baby Steps/ex4-string-methods.py
@@ -0,0 +1,8 @@
+name = "ada lovelace"
+# testing string (str) methods (Own! That's a comment)
+print(name)
+print(name.capitalize())
+print(name.title())
+print(name.upper())
+print(name.lower())
+# there are several methods, test!
