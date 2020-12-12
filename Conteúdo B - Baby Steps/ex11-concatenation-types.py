bb1d1cdc9926e8047e27160e428899056b1272de
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:52:44 -0300

    Create ex11

---
+++ Conteúdo B - Baby Steps/ex11-concatenation-types.py
@@ -0,0 +1,12 @@
+full_name = 'alejandro antonio silva junior silva'
+age = 20
+weight = 78.94
+height = 1.77
+
+data = 'Full Name: ' + full_name.title() + '\n'
+data = data + 'Age: ' + str(age) + '\n'
+data = data + 'Weight: ' + str(weight) + '\n'
+data = data + 'Height: ' + str(height)
+
+print('=== Data ===')
+print(data)
