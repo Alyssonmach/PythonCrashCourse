1fe5dd7ec7fa413a8c995dd71fd42ea197c0a600
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:51:36 -0300

    Create ex2

---
+++ Conteúdo B - Baby Steps/ex2-variable-hello-world.py
@@ -0,0 +1,5 @@
+message = "Hello Python World!"
+print("The message is: {}".format(message))
+print("The type: {}".format(type(message)))
+message = message.__add__(" Another message...")
+print(message)
