4c0e5a9c859aebcf65c888fe3ca1f52c57b7037a
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:52:24 -0300

    Create ex8

---
+++ Conteúdo B - Baby Steps/ex8-numbers-operations.py
@@ -0,0 +1,6 @@
+equation = ((((2 + 3) * 7) / 2) ** 2) // 1
+print('The result of equation is {}'.format(equation))
+print('The type is {}'.format(type(equation)))
+
+equation = ((((2 + 3) * 7) / 2) ** 2) 
+print('The result of equation is {:.4f}'.format(equation))
