788de0654984d8ffa0050ee44fec5b278c35985e
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:51:42 -0300

    Create ex3

---
+++ Conteúdo B - Baby Steps/ex3-strings-tests.py
@@ -0,0 +1,4 @@
+print('I told my friend, "Python is my favorite language!"')
+print("The language 'Python' is named after Monty Python, not the snake.")
+print("One of Python's strengths is its diverse and supportive community.")
+
