6880bb69904826abe2d0949172c32225f72ef884
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 18:52:13 -0300

    Create ex7

---
+++ Conteúdo B - Baby Steps/ex7-stripping-whitespaces.py
@@ -0,0 +1,9 @@
+first_name = 'ada '
+last_name = 'lovelace'
+
+full_name = (first_name.capitalize()).rstrip() + " " + last_name.capitalize()
+print(full_name)
+
+full_name = first_name.capitalize() + " " + last_name.capitalize()
+print(full_name)
+
