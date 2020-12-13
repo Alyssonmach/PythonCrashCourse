408402996537ce1b07ff72799cd2ecd26829cc77
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 22:02:33 -0300

    Create ex15

---
+++ content-c-lists/ex15_adding_list.py
@@ -0,0 +1,15 @@
+from motorcycles import motorcycles
+
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda'.capitalize())
+motorcycles.append('yamaha'.capitalize())
+motorcycles.append('suzuki'.capitalize())
+print(motorcycles)
