6edec62a0188043e54a5bb85a47b351e5e124cff
Alyssonmach <alyssonmachado388@gmail.com>
2020-12-12 21:39:12 -0300

    Create ex12

---
+++ content-c-lists/ex12_bicycles.py
@@ -0,0 +1,12 @@
+# list is a collection of items in a particular order
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+print('{}, {}, {}'.format(bicycles[0].capitalize(), bicycles[1].capitalize(), 
+                          bicycles[2].capitalize(), bicycles[3].capitalize()))
+
+print(bicycles[0:4])
+
+# las element
+print(bicycles[-1])
